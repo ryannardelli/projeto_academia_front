727df5b1c5cd99299991ddafd52a0ff0bdebf6c3
Ryan <ryannarelli12@gmail.com>
2025-06-20 15:31:18 -0300

    feat: Enviando primeira seção hero da landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeaderMain from "./components/HeaderMain";
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
     <div>
       <HeaderMain />
+      <HeroSection/>
     </div>
   );
 }
