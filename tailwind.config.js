b694257ed7fe081d3945a3ecddfee8e144dd0a37
Ryan <ryannarelli12@gmail.com>
2025-06-19 23:02:34 -0300

    feat: Enviando header inicial da landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: [
-     './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
@@ -10,6 +10,21 @@ module.exports = {
         inter: ['Inter', 'serif'],
         poppins: ['Poppins', 'sans-serif'],
         jakarta: ['Plus Jakarta Sans', 'sans-serif'],
+      },
+
+      fontSize: {
+        h1: '50px',
+        h2: '40px',
+        h3: '30px',
+        h4: '25px',
+        h5: '20px',
+        h6: '16px',
+      },
+
+      colors: {
+        primary: '#0D0D0D',
+        secondary: '#D90404',
+        assets: '#D9042B',
       },
     },
   },
