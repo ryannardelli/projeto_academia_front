05915399cc3625e30a313df3cbf236bd21d884a3
Ryan <ryannarelli12@gmail.com>
2025-06-20 19:04:31 -0300

    feat: Enviando nova seção e relizando alguns ajustes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturesSection from "./components/FeaturesSection";
 import HeaderMain from "./components/HeaderMain";
 import HeroSection from "./components/HeroSection";
+import OnlineAcademySection from "./components/OnlineAcademySection";
 import Testimonial from "./components/Testimoinal";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <HeaderMain />
       <HeroSection/>
       <FeaturesSection />
+      <OnlineAcademySection />
       <Testimonial />
     </div>
   );
